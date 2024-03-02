c5bf9ace8e6f55caf88bfb7a3cc21f24f4192953
Tim Hinh <timothyhinh79@gmail.com>
2024-03-01 18:25:42 -0800

    add db_credentials class

---
+++ cloud_functions/parse_email/classes/db_credentials.py
@@ -0,0 +1,7 @@
+class DBCredentials:
+    def __init__(self, host, port, user, password, database):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database = database
