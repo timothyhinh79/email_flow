37d6d95dbae5f6d3ec29d78d6c1f50b844e34805
Tim Hinh <timothyhinh79@gmail.com>
2024-04-21 13:04:36 -0700

    add logic to create token for google drive

---
+++ google_drive/create_token.py
@@ -0,0 +1,25 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# Define access scopes to be granted for Google Drive API
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+# Define client_secrets_json containing Client ID + Client Secret
+client_secrets_json = "credentials/client_secrets.json"
+
+# Define where token.json should be generated, containing the Refresh Token
+token_json = "credentials/token.json"
+
+# Invoke OAuth2 Flow to securely grant permission for Google Drive API scoped access
+flow = InstalledAppFlow.from_client_secrets_file(
+    client_secrets_json, SCOPES
+)
+creds = flow.run_local_server(port=0)
+
+with open(token_json, "w") as token:
+    token.write(creds.to_json())
